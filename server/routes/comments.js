7fa2f5dc732cec47cda6ea2f4321c84271cc874c
Salma EL HAZAL <salma.elhazal@gmail.com>
2024-07-02 11:14:40 +0100

    some changes

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
   getComments,
   addComment,
-  deleteComment,
 } from "../controllers/comments.js";
 import { verifyToken } from "../middleware/auth.js";
 
