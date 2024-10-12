0e5e7fecea1e7eea70562ed376b9be3baa0c5d2e
Salma EL HAZAL <salma.elhazal@gmail.com>
2024-10-12 11:14:08 +0100

    some posting changes

---
+++ client/src/scenes/widgets/MyExPost.jsx
@@ -0,0 +1,429 @@
+import * as React from "react";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  AttachFileOutlined,
+  GifBoxOutlined,
+  ImageOutlined,
+  MicOutlined,
+  MoreHorizOutlined,
+} from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Typography,
+  InputBase,
+  useTheme,
+  Button,
+  IconButton,
+  useMediaQuery,
+} from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import Dropzone from "react-dropzone";
+import UserImage from "../../components/UserImage";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../../state/index";
+
+import PropTypes from "prop-types";
+import { Dropdown } from "@mui/base/Dropdown";
+import { Menu } from "@mui/base/Menu";
+import { MenuButton as BaseMenuButton } from "@mui/base/MenuButton";
+import { MenuItem as BaseMenuItem, menuItemClasses } from "@mui/base/MenuItem";
+import { styled } from "@mui/system";
+import { CssTransition } from "@mui/base/Transitions";
+import { PopupContext } from "@mui/base/Unstable_Popup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFutbol,
+  faBriefcase,
+  faHandsHelping,
+} from "@fortawesome/free-solid-svg-icons";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useTranslation } from 'react-i18next';
+
+
+
+const MyPostWidget = ({ picturePath }) => {
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+  const [posttype, setPostType] = useState("");
+  const { palette } = useTheme();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const mediumMain = palette.neutral.mediumMain;
+  const medium = palette.neutral.medium;
+  const { t } = useTranslation();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (type) => {
+    setPostType(type);
+    setAnchorEl(null);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append("userId", _id);
+    formData.append("description", post);
+    formData.append("posttype", posttype);
+    if (image) {
+      formData.append("picture", image);
+      formData.append("picturePath", image.name);
+    }
+
+    const response = await fetch(`http://localhost:3001/posts`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+      body: formData,
+    });
+    toast.success("Post Added successfully");
+    const posts = await response.json();
+    dispatch(setPosts({ posts }));
+    setImage(null);
+    setPost("");
+    setPostType("");
+  };
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween gap="1.5rem">
+        <UserImage image={picturePath} />
+        <InputBase
+          placeholder= { t ("What's on your mind...")}
+          onChange={(e) => setPost(e.target.value)}
+          value={post}
+          sx={{
+            width: "100%",
+            backgroundColor: palette.neutral.light,
+            borderRadius: "2rem",
+            padding: "1rem 2rem",
+          }}
+        />
+
+      </FlexBetween>
+      {isImage && (
+        <Box
+          border={`1px solid ${medium}`}
+          borderRadius="5px"
+          mt="1rem"
+          p="1rem"
+        >
+          <Dropzone
+            acceptedFiles=".jpg,.jpeg,.png"
+            multiple={false}
+            onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <FlexBetween>
+                <Box
+                  {...getRootProps()}
+                  border={`2px dashed ${palette.primary.main}`}
+                  p="1rem"
+                  width="100%"
+                  sx={{ "&:hover": { cursor: "pointer" } }}
+                >
+                  <input {...getInputProps()} />
+                  {!image ? (
+                    <p> { t ("Add Image Here")}</p>
+                  ) : (
+                    <FlexBetween>
+                      <Typography>{image.name}</Typography>
+                      <EditOutlined />
+                    </FlexBetween>
+                  )}
+                </Box>
+                {image && (
+                  <IconButton
+                    onClick={() => setImage(null)}
+                    sx={{ width: "15%" }}
+                  >
+                    <DeleteOutlined />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            )}
+          </Dropzone>
+        </Box>
+      )}
+
+      <Divider sx={{ margin: "1.25rem 0" }} />
+
+      <FlexBetween>
+        <Dropdown>
+          <MenuButton onClick={handleMenuClick}>
+            <FontAwesomeIcon
+              icon={faBriefcase}
+              style={{ marginRight: "8px", marginLeft: "8px" , color: "#3ABEF9" }}
+            />
+            { t ("Professionnel")}
+          </MenuButton>
+
+          <Menu slots={{ listbox: AnimatedListbox }}>
+            <MenuItem onClick={() => {setIsImage(!isImage); handleMenuItemClick('💼Professionnel Post')}}>
+            { t ("Offre de Stage & Emploi")}
+            </MenuItem>
+            <MenuItem onClick={() => {setIsImage(!isImage); handleMenuItemClick('💼Professionnel Post')}}>{ t ("Événement")}</MenuItem>
+            <MenuItem onClick={() => {setIsImage(!isImage); handleMenuItemClick('💼Professionnel Post')}}>
+              {" "}
+              { t ("Programmes & Formation")}
+            </MenuItem>
+          </Menu>
+        </Dropdown>
+
+        <Dropdown>
+          <MenuButton onClick={handleMenuClick}>
+            <FontAwesomeIcon
+              icon={faFutbol}
+              style={{ marginRight: "8px", marginLeft: "8px"  , color: "#65B741" }}
+            />
+            { t ("Sport")}
+          </MenuButton>
+          <Menu slots={{ listbox: AnimatedListbox }}>
+            <MenuItem onClick={() => {setIsImage(!isImage); handleMenuItemClick('⚽Sport Post')}}>
+            { t ("Organiser Un Match")}
+            </MenuItem>
+            <MenuItem onClick={() => {setIsImage(!isImage); handleMenuItemClick('⚽Sport Post')}}>{ t ("Événement")}</MenuItem>
+            <MenuItem onClick={() => {setIsImage(!isImage); handleMenuItemClick('⚽Sport Post')}}>{ t ("Olumpiades")}</MenuItem>
+          </Menu>
+        </Dropdown>
+
+        <Dropdown>
+          <MenuButton onClick={handleMenuClick}>
+            <FontAwesomeIcon
+              icon={faHandsHelping}
+              style={{ marginRight: "8px", marginLeft: "8px"  , color: "#EF5A6F" }}
+            />
+            { t ("Social")}
+          </MenuButton>
+          <Menu slots={{ listbox: AnimatedListbox }}>
+            <MenuItem onClick={() => {setIsImage(!isImage); handleMenuItemClick('🤝Social Post')}}>{ t ("Donation")}</MenuItem>
+            <MenuItem onClick={() => {setIsImage(!isImage); handleMenuItemClick('🤝Social Post')}}>{ t ("Événement")}</MenuItem>
+            <MenuItem onClick={() => {setIsImage(!isImage); handleMenuItemClick('🤝Social Post')}}>{ t ("Aide")}</MenuItem>
+          </Menu>
+        </Dropdown>
+
+        <FlexBetween
+          gap="0.25rem"
+          onClick={() => setIsImage(!isImage)}
+        ></FlexBetween>
+        {/*<MenuTransitions2 />
+        <MenuTransitions />
+        <MenuTransitions3 />*/}
+
+        {isNonMobileScreens ? (
+          <>
+            {/*<FlexBetween gap="0.25rem">
+              <GifBoxOutlined sx={{ color: mediumMain }} />
+              <Typography color={mediumMain}>Clip</Typography>
+            </FlexBetween>
+
+            <FlexBetween gap="0.25rem">
+              <AttachFileOutlined sx={{ color: mediumMain }} />
+              <Typography color={mediumMain}>Attachment</Typography>
+            </FlexBetween>
+
+            <FlexBetween gap="0.25rem">
+              <MicOutlined sx={{ color: mediumMain }} />
+              <Typography color={mediumMain}>Audio</Typography>
+            </FlexBetween>*/}
+          </>
+        ) : (
+          <FlexBetween gap="0.25rem">
+            <MoreHorizOutlined sx={{ color: mediumMain }} />
+          </FlexBetween>
+        )}
+
+        <Button
+          disabled={!post}
+          onClick={handlePost}
+          sx={{
+            color: palette.background.alt,
+            backgroundColor: palette.primary.main,
+            borderRadius: "3rem",
+          }}
+        >
+          { t ("POST")}
+        </Button>
+      </FlexBetween>
+    </WidgetWrapper>
+  );
+};
+
+export default MyPostWidget;
+
+////
+const blue = {
+  50: "#F0F7FF",
+  100: "#C2E0FF",
+  200: "#99CCF3",
+  300: "#66B2FF",
+  400: "#3399FF",
+  500: "#007FFF",
+  600: "#0072E6",
+  700: "#0059B3",
+  800: "#004C99",
+  900: "#003A75",
+};
+
+const grey = {
+  50: "#F3F6F9",
+  100: "#E5EAF2",
+  200: "#DAE2ED",
+  300: "#C7D0DD",
+  400: "#B0B8C4",
+  500: "#9DA8B7",
+  600: "#6B7A90",
+  700: "#434D5B",
+  800: "#303740",
+  900: "#1C2025",
+};
+const MenuContainer = styled("div")(
+  ({ theme }) => `
+  border: 2px solid ${theme.palette.mode === "dark" ? blue[500] : blue[300]};
+  border-radius: 12px;
+  padding: 4px;
+  display: inline-block;
+  `
+);
+const Listbox = styled("ul")(
+  ({ theme }) => `
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 0.875rem;
+  box-sizing: border-box;
+  padding: 6px;
+  margin: 12px 0;
+  min-width: 200px;
+  border-radius: 12px;
+  overflow: auto;
+  outline: 0px;
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  box-shadow: 0px 4px 30px ${
+    theme.palette.mode === "dark" ? grey[900] : grey[200]
+  };
+  z-index: 1;
+
+  .closed & {
+    opacity: 0;
+    transform: scale(0.95, 0.8);
+    transition: opacity 200ms ease-in, transform 200ms ease-in;
+  }
+  
+  .open & {
+    opacity: 1;
+    transform: scale(1, 1);
+    transition: opacity 100ms ease-out, transform 100ms cubic-bezier(0.43, 0.29, 0.37, 1.48);
+  }
+
+  .placement-top & {
+    transform-origin: bottom;
+  }
+
+  .placement-bottom & {
+    transform-origin: top;
+  }
+  `
+);
+
+const AnimatedListbox = React.forwardRef(function AnimatedListbox(props, ref) {
+  const { ownerState, ...other } = props;
+  const popupContext = React.useContext(PopupContext);
+
+  if (popupContext == null) {
+    throw new Error(
+      "The `AnimatedListbox` component cannot be rendered outside a `Popup` component"
+    );
+  }
+
+  const verticalPlacement = popupContext.placement.split("-")[0];
+
+  return (
+    <CssTransition
+      className={`placement-${verticalPlacement}`}
+      enterClassName="open"
+      exitClassName="closed"
+    >
+      <Listbox {...other} ref={ref} />
+    </CssTransition>
+  );
+});
+
+AnimatedListbox.propTypes = {
+  ownerState: PropTypes.object.isRequired,
+};
+
+const MenuItem = styled(BaseMenuItem)(
+  ({ theme }) => `
+  list-style: none;
+  padding: 8px;
+  border-radius: 8px;
+  cursor: default;
+  user-select: none;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+
+  &.${menuItemClasses.focusVisible} {
+    outline: 3px solid ${theme.palette.mode === "dark" ? blue[600] : blue[200]};
+    background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  }
+
+  &.${menuItemClasses.disabled} {
+    color: ${theme.palette.mode === "dark" ? grey[700] : grey[400]};
+  }
+
+  &:hover:not(.${menuItemClasses.disabled}) {
+    background-color: ${theme.palette.mode === "dark" ? blue[900] : blue[50]};
+    color: ${theme.palette.mode === "dark" ? blue[100] : blue[900]};
+  }
+  `
+);
+
+const MenuButton = styled(BaseMenuButton)(
+  ({ theme }) => `
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: 600;
+  font-size: 0.875rem;
+  line-height: 1.5;
+  padding: 8px 16px;
+  border-radius: 8px;
+  color: white;
+  transition: all 150ms ease;
+  cursor: pointer;
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+  color: ${theme.palette.mode === "dark" ? grey[200] : grey[900]};
+  box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+
+  &:hover {
+    background: ${theme.palette.mode === "dark" ? grey[800] : grey[50]};
+    border-color: ${theme.palette.mode === "dark" ? grey[600] : grey[300]};
+  }
+
+  &:active {
+    background: ${theme.palette.mode === "dark" ? grey[700] : grey[100]};
+  }
+
+  &:focus-visible {
+    box-shadow: 0 0 0 4px ${
+      theme.palette.mode === "dark" ? blue[300] : blue[200]
+    };
+    outline: none;
+  }
+  `
+);
